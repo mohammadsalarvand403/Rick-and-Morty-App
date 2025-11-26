8e153b1e678d3cfda3bc8c8336130494c4309fec
mohammadsalarvand <mmdslv7707@gmail.com>
2025-11-26 22:34:50 +0330

    created component search & searchQueryin in data

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,41 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import CharecterDetil from "./components/CharacterDeteil";
 import CharecterList from "./components/CharacterList";
-import Navbar, { SearchResult } from "./components/Navbar";
-import { toast } from "react-hot-toast";
+import Navbar, { Search, SearchResult } from "./components/Navbar";
+import { toast, Toaster } from "react-hot-toast";
 import axios from "axios";
 
 function App() {
   const [characters, setCharaters] = useState([]);
   const [isLoding, setIsLoding] = useState(false);
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     async function FetcheData() {
       try {
         setIsLoding(true);
         const { data } = await axios.get(
-          "https://rickandmortyapi.com/api/character"
+          `https://rickandmortyapi.com/api/character?name=${query}`
         );
         setCharaters(data.results.slice(0, 5));
       } catch (err) {
+        setCharaters([]);
         toast.error(err.response.data.error);
       } finally {
         setIsLoding(false);
       }
     }
+    // if (query.length < 3) {
+    //   setCharaters([]);
+    //   return;
+    // }
     FetcheData();
-  }, []);
+  }, [query]);
   return (
     <div className="app">
+      <Toaster />
       <Navbar>
+        <Search quary={query} setQuery={setQuery} />
         <SearchResult numOfResult={characters.length} />
       </Navbar>
       <Main characters={characters}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import { HeartIcon } from "@heroicons/react/24/outline";
-import { Children } from "react";
-function Navbar({ Children }) {
+export default function Navbar({ children }) {
   return (
     <nav className="navbar">
       <div className="navbar__logo">LOGO💕</div>
-      <input type="text" className="text-field" placeholder="searche...." />
-      {Children}
+      {children}
       <button className="heart">
         <HeartIcon className="icon" />
         <span className="badge">3</span>
@@ -14,8 +12,18 @@ function Navbar({ Children }) {
   );
 }
 
-export default Navbar;
-
 export function SearchResult({ numOfResult }) {
   return <div className="navbar__result"> found {numOfResult} charecter</div>;
+}
+
+export function Search({ quary, setQuery }) {
+  return (
+    <input
+      value={quary}
+      onChange={(e) => setQuery(e.target.value)}
+      type="text"
+      className="text-field"
+      placeholder="searche...."
+    />
+  );
 }
